6c5883a89cb20f30f3f516c693071d7f7615e501
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 14:56:12 +0200

    correction ajout HTML

--- exercice1.7.php
+++ exercice1.7.php
@@ -1,6 +1,18 @@
 <?php
-$lastname = "Baggins";
-$firstname = "Frodo";
-$age = 50;
-echo 'Bonjour '.$lastname.' '.$firstname.', tu as '.$age.' ans.';
+    $lastname = 'Baggins';
+    $firstname = 'Frodo';
+    $age = 50;
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1.7</title>
+</head>
+<body>
+    <h1>Exercice 7 Partie 1 : Les variables PHP</h1>
+    <p><?= 'Bonjour '.$lastname.' '.$firstname.', tu as '.$age.' ans.';?></p>
+</body>
+</html>
